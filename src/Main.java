9532bcbc2ad22012a8921a09523ec010e95c36b9
StalinInDaHouse <72594243+StalinInDaHouse@users.noreply.github.com>
2022-03-09 17:33:28 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person("Pikalov", "Dmitry", "Alexandrovich","24");
+        person1.printAge();
+    }
+}
