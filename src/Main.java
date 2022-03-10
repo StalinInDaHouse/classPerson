0de4d331fa905d0fdb75296b9c24a0f813d63ab4
StalinInDaHouse <72594243+StalinInDaHouse@users.noreply.github.com>
2022-03-10 15:05:23 +0700

    upd

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Person person1 = new Person("Pikalov", "Dmitry", "Alexandrovich","24");
-        person1.printAge();
+        person1.printBirthYear();
     }
 }
